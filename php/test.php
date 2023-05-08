4615369f1f196c029738c36ebee7115a56a8ce2f
Zakaria Kaichouhi <2169392@talnet.nl>
2023-05-08 11:01:37 +0200

    first commit

---
+++ php/test.php
@@ -0,0 +1,23 @@
+<?php
+$getal1 = 12;
+$getal2 = 10;
+
+echo $getal1 + $getal2;
+
+echo "<br>";
+
+$getal1 = 12;
+$getal2 = 10;
+
+echo $getal1 * $getal2;
+
+echo "<br>";
+$naam="Zaka";
+echo "Welkom".$naam;
+
+echo "<br>";
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+?>
