0fb1aced5ffed7d093e892bb350019bc9dfb672e
Zaka2005 <131659914+Zaka2005@users.noreply.github.com>
2023-05-23 08:46:28 +0200

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,36 @@
+<?php
+//Deel1
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+//$myArray[] = 'boot';
+
+//foreach ($myArray as $value) {
+//echo $value . "<br>";
+//}
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];  
+
+//Deel 2
+//$myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+//$elementen = count($myArray);
+//echo "1. Het array heeft $elementen elementen.\n";
+ 
+//$myArray[] = 'boot'; // Een element toevoegen aan het array
+//$elementen = count($myArray);
+//echo "1. Het array heeft $elementen elementen.";
+
+//deel3
+// bar
+// 4
+// toyota
+// cars
+// 5
+//laaste elemanet van het array
+
+//deel4
+ $cijfersPHP = [4.4, 4.6, 5.6, 6.1, 7.6, 7.2];
+$gemiddelde = array_sum($cijfersPHP) / count($cijfersPHP);
+
+echo $gemiddelde;
+echo(round(5.9166666666667) . "<br>");
+
+?>
+
